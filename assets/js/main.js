0ea0a353a1166c9502c1f6ce1069d47732015123
Netrvin <131@moe.im>
2020-04-20 16:03:27 +0800

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,13 +36,13 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.webp': 'center',
-							'images/bg02.webp': 'center',
-							'images/bg03.webp': 'center'
+							'images/bg01.jpg': 'center',
+							'images/bg02.jpg': 'center',
+							'images/bg03.jpg': 'center'
 						},
 
 					// Delay.
-						delay: 60000
+						delay: 30000
 
 				};
 
